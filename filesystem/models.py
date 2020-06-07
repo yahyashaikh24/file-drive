f0f53b007e1d90dd32d01184b03ad36875cbfa95
Yahya Shaikh <yshaikh2424@gmail.com>
2020-06-07 23:27:24 +0530

    Alter field and remove None from field

--- filesystem/models.py
+++ filesystem/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 class FilesUpload(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file_field = models.FileField(unique=True)
-    file_path = models.FilePathField(path=None)
+    file_path = models.FilePathField()
     thumbnail_img = models.ImageField(null=True)
+    create_date = models.DateTimeField(default=datetime.datetime.now())
+    modified_date = models.DateTimeField(default=datetime.datetime.now())
